e89543e52ce8f8d2986e7904d8c8193ca702e939
Bethany Janos <bpjanos@ncsu.edu>
2018-03-22 23:00:08 -0400

    Reduced comparisons when finding the median, fixed it not working on even numbers. Insertion sort needs to be fixed.

--- src/QuickSelect.java
+++ src/QuickSelect.java
@@ -22,19 +22,20 @@ public class QuickSelect extends Sorter {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		integerList = new ArrayList<>();
-//		integerList.add(10);
-//		integerList.add(4);
-//		integerList.add(5);
-//		integerList.add(8);
-//		integerList.add(6);
-//		integerList.add(11);
-//		integerList.add(26);
-//		integerList.add(32);
-//		integerList.add(2);
-//		integerList.add(9);
+		integerList = new ArrayList<>();
+		integerList.add(10);
+		integerList.add(4);
+		integerList.add(5);
+		integerList.add(8);
+		integerList.add(6);
+		integerList.add(11);
+		integerList.add(26);
+		integerList.add(32);
+		integerList.add(2);
+		integerList.add(9);
+		integerList.add(22);
 
-		read();
+//		read();
 		int mid = (int) Math.floor((integerList.size() - 1) / 2);
 		startTime = System.nanoTime();
 		int k = quickselect(integerList, 0, integerList.size() - 1, mid);
@@ -88,8 +89,8 @@ public class QuickSelect extends Sorter {
 	public static int quickselect(ArrayList<Integer> list, int left, int right, int k) {
 		if (right - left > 1) {
 			int pivotIndex;
-			if(list.size() >= 9) {
-				int mid = (int) Math.floor((list.size() + 1) / 2);
+			if(right - left >= 9) {
+				int mid = (int) Math.floor((left + right) / 2);
 				pivotIndex = median(left, list.get(left), right, list.get(right), mid, list.get(mid));
 				int temp = list.get(pivotIndex);
 				list.set(pivotIndex, list.get(right));
@@ -103,24 +104,44 @@ public class QuickSelect extends Sorter {
 			} else if (k < pivotIndex) {
 				return quickselect(list, left, right - 1, k);
 			} else {
-				return quickselect(list, left + 1, right, k - pivotIndex);
+				return quickselect(list, left + 1, right, k);
 			}
 
 		} else {
-			ArrayList<Integer> sorted = InsertionSort.nonRecursiveInsertionSort(list);
-			int mid = (int) Math.floor((sorted.size() - 1) / 2);
-			return sorted.get(mid);
+			
+			return insertionSortMedian(list, left, right);
+//			int mid = (int) Math.floor((sorted.size() - 1) / 2);
+//			return sorted.get(mid);
 		}
 	}
 
 	public static int median(int left, int leftVal, int right, int rightVal, int middle, int middleVal) {
-		if(comp.compare(rightVal, leftVal) == comp.compare(middle, rightVal)) {
+		int rtol = comp.compare(rightVal, leftVal);
+		if(rtol == comp.compare(middle, rightVal)) {
 			return right;
-		} else if (comp.compare(leftVal, rightVal) == comp.compare(middle, leftVal)) {
+		} else if (rtol*(-1) == comp.compare(middle, leftVal)) {
 			return left;
 		} else {
 			return middle;
 		}
+	}
+	
+	public static int insertionSortMedian(ArrayList<Integer> L, int left, int right) {
+		/*
+		 * One element is removed and then compared to each element until its proper
+		 * place is found. Its correct placement is when the element is less than the
+		 * one it is being compared to. It will then be placed at that index.
+		 */
+		for (int j = left; j <= right; j++) {
+			int key = L.get(j);
+			int i = j - 1;
+			while (i >= left && comp.compare(L.get(i), key) > 0) {
+				L.set(i + 1, L.get(i));
+				i = i - 1;
+			}
+			L.set(i + 1, key);
+		}
+		return L.get((int) Math.floor((left + right)/2));
 	}
 
 }
